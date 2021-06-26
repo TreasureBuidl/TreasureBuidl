1b3ee3f92eabcce0c412c40c3ee3593ededbbc12
Jason Chitla <jasonchitla@hotmail.com>
2021-06-26 15:07:37 -0400

    updated tailwind config

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      darkBlue: '#212B38',
+      darkerBlue: '#1E272E',
+      purple: '#726EFF',
+      white: '#FFFFFF',
+      offWhite: '#C4C3D0',
+      aave: '#B6509E',
+      uniswap: '#FF007A',
+      compound: '#00D395'
+    },
+    fontFamily: {
+      body: ['Open Sans']
+    },
     extend: {},
   },
   variants: {
