1a770592e61da139210e912a14322be6c8a2acbc
Saarule <saaryazdi@gmail.com>
2021-06-28 15:42:30 +0300

    Basic wallet connection using useWallet()

---
+++ packages/frontend/components/Connection/Connection.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useWallet, UseWalletProvider } from 'use-wallet'
+
+function Connection() {
+    const wallet = useWallet()
+
+    const connectWallet = async (e) => {
+        e.preventDefault()
+        await wallet.connect()
+    }
+    return (
+        <div>
+            <button onClick={connectWallet}>Connect Wallet!</button>
+        </div>
+    )
+}
+
+export default () => (
+    <UseWalletProvider
+        chainId={1}
+        connectors={{
+            // This is how connectors get configured
+            portis: { dAppId: 'my-dapp-id-123-xyz' },
+            //provided: {provider: window.cleanEthereum},
+        }}
+    >
+        <Connection />
+    </UseWalletProvider>
+)
