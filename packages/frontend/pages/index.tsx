c2d5edd42cade06d8c0337a5bda8aef6df24d136
Jason Chitla <jasonchitla@hotmail.com>
2021-06-29 01:13:09 -0400

    adding action cards row

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,9 +1,11 @@
-import NavBar
- from "@components/NavBar"
+import NavBar from "@components/NavBar"
+import ActionsRow from "@components/ActionsRow"
+
 export default function Home() {
   return (
     <div className="bg-darkBlue h-screen w-screen">
       <NavBar/>
+      <ActionsRow/>
     </div>
   )
 }
