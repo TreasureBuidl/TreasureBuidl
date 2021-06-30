09d23b11cfec1b8f14f1282c4f15bfd7e6f869fd
Veronica <veronica.21.coutts@gmail.com>
2021-07-01 00:02:19 +0200

    Treasure Maps complete

--- packages/onchain/contracts/treasure-maps/TreasureMaps.sol
+++ packages/onchain/contracts/treasure-maps/TreasureMaps.sol
@@ -19,10 +19,10 @@ contract TreasureMaps is ModifiedErc721 {
     uint256 public tokenCount_;
 
     // Storage for the two token types within this system.
-    bytes32 constant TREASURE_MAP = bytes32(keccak256("TREASURE_MAP"));
+    bytes32 constant OUTLINE = bytes32(keccak256("OUTLINE"));
         // Treasure maps contain the targets and target function signatures.
     // Storage for treasure map information.
-    struct TreasureOutline {
+    struct Outline {
         // The address that added this treasure map.
         address creator;
         // Array of target contract addresses.
@@ -36,14 +36,12 @@ contract TreasureMaps is ModifiedErc721 {
         // // If the map has a fee.
         // uint256 mapFee;
     }
-    // Token IDs (type TREASURE_MAP) to treasure map instructions.
-    mapping(uint256 => TreasureOutline) public TreasureOutlines_;
-    // TODO Make treasure outlines something you need to specifically make 
-    // not the default thing created
+    // Token IDs (type OUTLINE) to treasure map instructions.
+    mapping(uint256 => Outline) public outlines_;
 
-    bytes32 constant COORDINATES = bytes32(keccak256("COORDINATES"));
-        // Coordinates contain the function parameters and call native values.
-    struct Coordinates {
+    bytes32 constant TREASURE_MAPS = bytes32(keccak256("TREASURE_MAPS"));
+        // Treasure Maps contain the function parameters and call native values.
+    struct TreasureMaps {
         // Array of target contract addresses.
         address[] callTargets;
         // Cost in native tokens to explore this map.
@@ -53,42 +51,29 @@ contract TreasureMaps is ModifiedErc721 {
         // Array of `msg.value`'s for target calls. 
         uint[] callValues;
     }
-    // Token IDs (type COORDINATES) to coordinate details.
-    mapping(uint256 => Coordinates) public treasureCoordinates_;
-    // TODO treasureMaps_
-    // TODO Make treasure coords treasure maps and the default. 
-    // will need to store all the info inside. 
-    // will need a conditional treasure outline param which will then 
-    // ignore passed in variables for the ones specified in the outline. 
+    // Token IDs (type TREASURE_MAPS) to coordinate details.
+    mapping(uint256 => TreasureMaps) public treasureMaps_;
+    // FUTURE A conditional treasure outline param which will then ignore passed
+    // in variables for the ones specified in the outline. 
+
+    event TreasureMapAdded(
+        address indexed creator,
+        uint256 indexed mapID,
+        string description
+    );
+
+    constructor() ModifiedErc721("Treasure Maps", "tMAP") {
+
+    }
 
     function getCoordinates(uint256 _ID) public view returns(
         address[] memory callTargets,
         uint256[] memory callValues,
         bytes[] memory callData
     ) {
-        callTargets = treasureCoordinates_[_ID].callTargets;
-        callValues = treasureCoordinates_[_ID].callValues;
-        callData = treasureCoordinates_[_ID].callData;
-    }
-
-    // event TreasureMapAdded(
-    //     address indexed creator,
-    //     uint256 indexed mapID,
-    //     string description,
-    //     address[] callTargets,
-    //     bytes[] callData,
-    //     uint[] callValues
-    // );
-
-    // event MapExplored(
-    //     address indexed explorer,
-    //     uint256 indexed mapID
-    // );
-
-    event Testing(uint256 mapID, uint256 coordinatesID);
-
-    constructor() ModifiedErc721("Treasure Maps", "tMAP") {
-
+        callTargets = treasureMaps_[_ID].callTargets;
+        callValues = treasureMaps_[_ID].callValues;
+        callData = treasureMaps_[_ID].callData;
     }
 
     /**
@@ -98,7 +83,6 @@ contract TreasureMaps is ModifiedErc721 {
      * @param   _callData Array of encoded parameters for function at target.
      * @param   _callValues Array of `msg.value`'s for target calls.
      * @return  treasureMapID The token ID for the treasure map created.
-     * @return  coordinatesID The token ID for the coordinates created.
      */
     function createTreasure(
         string calldata _description,
@@ -108,50 +92,39 @@ contract TreasureMaps is ModifiedErc721 {
         uint[] calldata _callValues
     )
         external
-        returns(uint256 treasureMapID, uint256 coordinatesID)
+        returns(uint256 treasureMapID)
     {
-        treasureMapID = _addMap(
+        treasureMapID = _addTreasureMap(
+            msg.sender,
+            _targetAddr,
+            _functionSig,
+            _callData,
+            _callValues
+        );
+        
+        emit TreasureMapAdded(
+            msg.sender,
+            treasureMapID,
+            _description
+        );
+    }
+
+    function createTreasureOutline(
+        string calldata _description,
+        address[] calldata _targetAddr,
+        string[] calldata _functionSig
+    )
+        external
+        returns(uint256 outlineID)
+    {
+        outlineID = _addOutline(
             msg.sender,
             _targetAddr,
             _functionSig
         );
-
-        coordinatesID = _addCoordinates(
-            treasureMapID,
-            msg.sender,
-            _callData,
-            _callValues
-        );
-        // TODO Emitting data. 
-        // emit Testing(treasureMapID, coordinatesID);
     }
 
-    function executeMap(
-        uint256 _treasureCoordinates
-    )
-        external
-    {
-        Coordinates memory coords = treasureCoordinates_[_treasureCoordinates];
-        
-        require(
-            coords.callTargets.length != 0,
-            "Map does not exist"
-        );
-
-        // Executing map instructions
-        for (uint i = 0; i < coords.callTargets.length; i++) {
-            (bool success, bytes memory returnData) = coords.callTargets[i].call{
-                    value: coords.callValues[i]
-                }(
-                    coords.callData[i]
-                );
-            require(success, "MAP: Exploration failed");
-        }
-
-        // TODO should emit return data?
-    }
-
-    function _addMap(
+    function _addOutline(
         address _creator,
         address[] calldata _callTargets,
         string[] calldata _functionSigs
@@ -167,34 +140,32 @@ contract TreasureMaps is ModifiedErc721 {
         tokenCount_ += 1;
         tokenID = tokenCount_;
 
-        TreasureOutlines_[tokenID] = TreasureOutline({
+        outlines_[tokenID] = Outline({
             creator: _creator,
             callTargets: _callTargets,
             callFunctionSigs: _functionSigs,
             explores: 0
         });
 
-        _mint(TREASURE_MAP, _creator, tokenID);
+        _mint(OUTLINE, _creator, tokenID);
     }
 
-    event Test(bytes[] encoded);
-
-    function _addCoordinates(
-        uint256 _treasureMap,
-        address _creator,
+    function _addTreasureMap(
+         address _creator,
+        address[] calldata _callTargets,
+        string[] calldata _functionSigs,
         bytes[] calldata _callData,
         uint256[] calldata _callValues
     )
         internal
         returns(uint256 tokenID)
     {
-        TreasureOutline memory map = TreasureOutlines_[_treasureMap];
-
-        require(map.creator != address(0), "Map does not exist");
+        require(_creator != address(0), "Map does not exist");
 
         require(
-            _callData.length == _callValues.length &&
-            map.callTargets.length == _callData.length,
+            _callTargets.length == _functionSigs.length &&
+            _callData.length == _functionSigs.length && 
+            _callValues.length == _functionSigs.length,
             "MAP: Array lengths differ"
         );
 
@@ -204,26 +175,24 @@ contract TreasureMaps is ModifiedErc721 {
         // Counter for map native token cost.
         uint256 callCost;
         // Storage for encoded function calls.
-        bytes[] memory generatedCallData = new bytes[](map.callTargets.length);
+        bytes[] memory generatedCallData = new bytes[](_callTargets.length);
         // Transforming data for efficient storage.
-        for (uint256 i = 0; i < map.callTargets.length; i++) {
+        for (uint256 i = 0; i < _callTargets.length; i++) {
             // Encoding function calls (signature and data)
             generatedCallData[i] = abi.encodePacked(
-                bytes4(keccak256(bytes(map.callFunctionSigs[i]))), 
+                bytes4(keccak256(bytes(_functionSigs[i]))), 
                 _callData[i]
             );
             callCost += _callValues[i];
         }
 
-        emit Test(generatedCallData);
-
-        treasureCoordinates_[tokenID] = Coordinates({
-            callTargets: map.callTargets,
+        treasureMaps_[tokenID] = TreasureMaps({
+            callTargets: _callTargets,
             callValueTotal: callCost,
             callData: generatedCallData,
             callValues: _callValues
         });
 
-        _mint(COORDINATES, _creator, tokenID);
+        _mint(TREASURE_MAPS, _creator, tokenID);
     }
 }
