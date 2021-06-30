04f32b7796ea71fd7524c70cf85344562b43733a
Veronica <veronica.21.coutts@gmail.com>
2021-07-01 00:38:33 +0200

    Treasure planet complete

---
+++ packages/onchain/contracts/treasure-plannet/ModifiedOwnership.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+/**
+ * @author  @vonie610 (Twitter & Telegram) | @Nicca42 (GitHub)
+ * @author  // TODO Thornton @??? | @thornm9 (GitHub)
+ * @notice  This contract has been modified away from the OZ standard Ownable in
+ *          order to allow ownership to be represented by an NFT token.
+ *          The owner of the NFT will have ownership rights over the owned 
+ *          contract.
+ * @dev     Contract module which provides a basic access control mechanism, 
+ *          where there is an account (an owner) that can be granted exclusive 
+ *          access to specific functions.
+ *          This module is used through inheritance. It will make available the 
+ *          modifier `onlyOwner`, which can be applied to your functions to 
+ *          restrict their use to the owner.
+ */
+abstract contract ModifiedOwnership is Context {
+    // Instance of the NFT token contract that represents ownership.
+    IERC721 internal ownerTokenInstance_;
+    // ID of the token that is the owner of contract.
+    uint256 private ownerTokenID_;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor(
+        address _ownerTokenInstance,
+        uint256 _ownerTokenID
+    ) internal {
+        ownerTokenID_ = _ownerTokenID;
+        ownerTokenInstance_ = IERC721(ownerTokenInstance_);
+        address currentOwner = ownerTokenInstance_.ownerOf(ownerTokenID_);
+        emit OwnershipTransferred(address(0), currentOwner);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return ownerTokenInstance_.ownerOf(ownerTokenID_);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    modifier onlyToken() {
+        require(
+            _msgSender() == address(ownerTokenInstance_),
+            "Ownable: ownership managed by token"
+        );
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyToken() returns(bool) {
+        require(
+            _msgSender() == address(ownerTokenInstance_),
+            "Renounce ownership through burn"
+        );
+        emit OwnershipTransferred(
+            ownerTokenInstance_.ownerOf(ownerTokenID_), 
+            address(0)
+        );
+
+        return true;
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(
+        address _newOwner
+    ) 
+        public 
+        virtual 
+        onlyToken() 
+        returns(bool) 
+    {
+        require(
+            _newOwner != address(0), 
+            "Ownable: new owner is the zero address"
+        );
+        address currentOwner = ownerTokenInstance_.ownerOf(ownerTokenID_);
+        emit OwnershipTransferred(currentOwner, _newOwner);
+
+        return true;
+    }
+}
--- packages/onchain/contracts/treasure-plannet/TreasurePlanet.sol
+++ packages/onchain/contracts/treasure-plannet/TreasurePlanet.sol
@@ -3,31 +3,63 @@
 pragma solidity 0.8.0;
 
 import "../treasure-maps/TreasureMaps.sol";
+import "./ModifiedOwnership.sol";
+// Receiver for ERC721
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+// Receiver for ERC1155
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-// TODO allows for delegated execution of maps with coordinates 
-
-contract TreasurePlanet {
+/**
+ * @author  @vonie610 (Twitter & Telegram) | @Nicca42 (GitHub)
+ * @notice  This contract allows for the storage of ERC 20 | 721 | 1155 so long
+ *          they do not require smart receiver hooks. 
+ *          // FUTURE allow for future standard hooks to be implemented?
+ */
+contract TreasurePlanet is ModifiedOwnership, IERC721Receiver, IERC1155Receiver {
     TreasureMaps public maps_;
 
-    constructor(address _mapToken) {
-        maps_ = TreasureMaps(_mapToken);
+    bytes4 private constant _INTERFACE_ID_ERC721 = bytes4(
+        keccak256(
+            "onERC721Received(address,address,uint256,bytes)"
+        )
+    );
+    bytes4 private constant _INTERFACE_ID_ERC1155 = bytes4(
+        keccak256(
+            "onERC1155Received(address,address,uint256,uint256,bytes)"
+        )
+    );
+    bytes4 private constant _INTERFACE_ID_ERC1155_BATCH = bytes4(
+        keccak256(
+            "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
+        )
+    );
+
+    /**
+     * @param   _treasureMaps Address of the treasure map contract. 
+     */
+    constructor(
+        address _treasureMaps,
+        address _ownerTokenInstance,
+        uint256 _ownerTokenID
+    ) 
+        ModifiedOwnership(
+            _ownerTokenInstance,
+            _ownerTokenID
+        )
+    {
+        maps_ = TreasureMaps(_treasureMaps);
     }
 
-    // function execute(uint256 _mapID) public {
-    //     // Doesn't work because storage layouts need to be idential in 
-    //     // delegatecall'ed contract 
-    //     maps_.delegatecall(
-    //         abi.encodeWithSignature(
-    //             "executeMap(uint256)", 
-    //             _mapID
-    //         )
-    //     );
-    // }
-
+    /**
+     * @param   _mapToken ID for the map token. 
+     * @notice  Allows the treasure planet to hold any asset that doesn't 
+     *          require specific smart contract receiver hooks. 
+     */
     function execute(
         uint256 _mapToken
     )
         external
+        onlyOwner()
     {
         (
             address[] memory callTargets,
@@ -49,5 +81,65 @@ contract TreasurePlanet {
                 );
             require(success, "MAP: Exploration failed");
         }
+    }
+
+    function supportsInterface(
+        bytes4 _interfaceId
+    ) 
+        external 
+        view 
+        override 
+        returns(bool) 
+    {
+        if(_interfaceId == _INTERFACE_ID_ERC721) {
+            return true;
+        } else if(_interfaceId == _INTERFACE_ID_ERC1155) {
+            return true;
+        } else if(_interfaceId == _INTERFACE_ID_ERC1155_BATCH) {
+
+        } else {
+            return false;
+        }
+    }
+
+    function onERC721Received(
+        address operator, 
+        address from, 
+        uint256 tokenId, 
+        bytes calldata data
+    ) 
+        external 
+        override
+        returns(bytes4) 
+    {
+        return _INTERFACE_ID_ERC721;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    )
+        external
+        override
+        returns(bytes4) 
+    {
+        return _INTERFACE_ID_ERC1155;
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    )
+        external
+        override
+        returns(bytes4)
+    {
+        return _INTERFACE_ID_ERC1155_BATCH;
     }
 }
