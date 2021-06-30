ec46a2f4cfe59bc68e2ad8e7f3f283446acd1595
Jason Chitla <jasonchitla@hotmail.com>
2021-06-30 02:10:48 -0400

    add protocol icon on top of custom card

--- packages/frontend/components/ActionsRow/ActionCard/index.tsx
+++ packages/frontend/components/ActionsRow/ActionCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Action, Protocol } from 'types/Treasure.types'
 import AmountInput from '@components/ActionsRow/ActionCard/AmountInput'
+import ProtocolIcon from '@components/ProtocolIcon'
 import useTreasure from 'hooks/useTreasure'
 import classnames from 'classnames'
 
@@ -8,6 +9,7 @@ function ActionCard({ action }: { action: Action }) {
   const { updateAction } = useTreasure();
 
   const getBackground = (protocol: Protocol): string => {
+    // #TODO: Add the rest of the protocol images
     switch (protocol) {
       case Protocol.Aave:
         return 'url(/images/cards/aaveCard.png)'
@@ -44,62 +46,67 @@ function ActionCard({ action }: { action: Action }) {
   const outputBoxHeight = 170
 
   return (
-    <div
-      className="-ml-5 bg-no-repeat bg-center"
-      style={{
-        backgroundImage: getBackground(action.type.protocol),
-        width: 315,
-        height: 373,
-      }}
-    >
-      <div
-        className={classnames(
-          'ml-4 mt-4 font-bold text-white text-center py-2 px-4 rounded-lg text-sm',
-          {
-            'bg-aave': action.type.protocol === Protocol.Aave,
-            'bg-compound': action.type.protocol === Protocol.Compound,
-            'bg-uniswap': action.type.protocol === Protocol.Uniswap,
-          }
-        )}
-        style={{ width: 244 }}
-      >
-        {action.type.operation}
+    <div className='flex flex-col'>
+      <div className="pr-14 pb-2 self-center">
+        <ProtocolIcon protocol={action.type.protocol} />
       </div>
-      <form>
-        <div className='flex flex-col'>
-          {action.input ? (
-            <div className='flex flex-col' style={{height: inputBoxHeight}}>
-              <div className='text-offWhite text-sm pl-4 pt-2'>
-                Input
-              </div>
-              <div style={{height: inputBoxHeight}}>
-                <AmountInput amount={action.input} handleChange={handleInputQuantityChange}/>
-              </div>
-            </div>
-          ) : (
-            <div style={{height: inputBoxHeight}}>
-            </div>
+      <div
+        className="-ml-5 bg-no-repeat bg-center"
+        style={{
+          backgroundImage: getBackground(action.type.protocol),
+          width: 315,
+          height: 373,
+        }}
+      >
+        <div
+          className={classnames(
+            'ml-4 mt-4 font-bold text-white text-center py-2 px-4 rounded-lg text-sm',
+            {
+              'bg-aave': action.type.protocol === Protocol.Aave,
+              'bg-compound': action.type.protocol === Protocol.Compound,
+              'bg-uniswap': action.type.protocol === Protocol.Uniswap,
+            }
           )}
-          <div className={classnames('ml-11', {
-            "bg-aave": action.type.protocol === Protocol.Aave,
-            "bg-compound": action.type.protocol === Protocol.Compound,
-            "bg-uniswap": action.type.protocol === Protocol.Uniswap
-            })} style={{width: 214, height: 2}}></div>
-          {action.output ? (
-            <div className='flex flex-col' style={{height: outputBoxHeight}}>
-              <div className='text-offWhite text-sm pl-4 pt-10'>
-                Output
-              </div>
-              <div style={{height: inputBoxHeight}}>
-                <AmountInput amount={action.output} handleChange={handleOutputQuantityChange}/>
-              </div>
-            </div>
-          ) : (
-            <div style={{height: outputBoxHeight}}>
-            </div>
-          )}
+          style={{ width: 244 }}
+        >
+          {action.type.operation}
         </div>
-      </form>
+        <form>
+          <div className='flex flex-col'>
+            {action.input ? (
+              <div className='flex flex-col' style={{height: inputBoxHeight}}>
+                <div className='text-offWhite text-sm pl-4 pt-2'>
+                  Input
+                </div>
+                <div style={{height: inputBoxHeight}}>
+                  <AmountInput amount={action.input} handleChange={handleInputQuantityChange}/>
+                </div>
+              </div>
+            ) : (
+              <div style={{height: inputBoxHeight}}>
+              </div>
+            )}
+            <div className={classnames('ml-11', {
+              "bg-aave": action.type.protocol === Protocol.Aave,
+              "bg-compound": action.type.protocol === Protocol.Compound,
+              "bg-uniswap": action.type.protocol === Protocol.Uniswap
+              })} style={{width: 214, height: 2}}></div>
+            {action.output ? (
+              <div className='flex flex-col' style={{height: outputBoxHeight}}>
+                <div className='text-offWhite text-sm pl-4 pt-10'>
+                  Output
+                </div>
+                <div style={{height: inputBoxHeight}}>
+                  <AmountInput amount={action.output} handleChange={handleOutputQuantityChange}/>
+                </div>
+              </div>
+            ) : (
+              <div style={{height: outputBoxHeight}}>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
