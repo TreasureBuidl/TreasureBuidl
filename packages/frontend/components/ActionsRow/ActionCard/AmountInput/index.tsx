5d55d3e1041435d5055fce4063a902aa5192bb21
Jason Chitla <jasonchitla@hotmail.com>
2021-06-30 22:21:18 -0400

    add dropdowns for token inputs

--- packages/frontend/components/ActionsRow/ActionCard/AmountInput/index.tsx
+++ packages/frontend/components/ActionsRow/ActionCard/AmountInput/index.tsx
@@ -1,17 +1,57 @@
 import React from 'react'
-import { Amount } from 'types/Treasure.types'
+import { Amount, Token } from 'types/Treasure.types'
 import TokenIcon from '@components/TokenIcon'
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
 
-function AmountInput({ amount, handleChange }: { amount: Amount, handleChange: (event: any) => void }) {
+const useStyles = makeStyles((theme) => ({
+  select: {
+    color: 'white',
+    '&:before': {
+        borderColor: 'white',
+    },
+    '&:after': {
+        borderColor: 'white',
+    },
+    maxWidth: 80,
+    width: 80,
+  },
+  icon: {
+      fill: 'white',
+  },
+  menu: {
+    backgroundColor: '#1E272E',
+  }
+}));
+
+function AmountInput({ amount, handleQuantityChange, handleTokenChange }: { amount: Amount, handleQuantityChange: (event: any) => void, handleTokenChange: (event: any) => void }) {
+  const classes = useStyles();
 
   return (
     <div className='flex flex-row ml-4 mt-4' style={{height: 32}}>
       <TokenIcon token={amount.token} />
       <div className='text-white font-medium text-lg pl-4'>
-        {amount.token}
+        <Select
+          value={amount.token}
+          onChange={handleTokenChange}
+          className={classes.select}
+          inputProps={{
+              classes: {
+                  icon: classes.icon,
+              },
+          }}
+          MenuProps={{
+            classes: {list: classes.menu},
+          }}
+        >
+          {Object.keys(Token).map((key) => (
+            <MenuItem value={Token[key]} key={key} style={{color: 'white', backgroundColor: '#1E272E'}}>{Token[key]}</MenuItem>
+          ))}
+        </Select>
       </div>
-      <input className="w-2/5 ml-6 py-1 px-2 text-white text-right leading-tight focus:outline-none" type="text" placeholder="1000.00"
-        style={{background: 'transparent'}} value={amount.quantity ?? ''} onChange={handleChange}/>
+      <input className="w-2/5 py-1 px-2 text-white text-right leading-tight focus:outline-none" type="text" placeholder="1000.00"
+        style={{background: 'transparent'}} value={amount.quantity ?? ''} onChange={handleQuantityChange}/>
     </div>
   )
 }
--- packages/frontend/components/ActionsRow/ActionCard/index.tsx
+++ packages/frontend/components/ActionsRow/ActionCard/index.tsx
@@ -42,6 +42,26 @@ function ActionCard({ action }: { action: Action }) {
     })
   }
 
+  const handleInputTokenChange = (event) => {
+    updateAction({
+      ...action,
+      input: {
+        ...action.input,
+        token: event.target.value,
+      }
+    })
+  }
+
+  const handleOutputTokenChange = (event) => {
+    updateAction({
+      ...action,
+      output: {
+        ...action.output,
+        token: event.target.value,
+      }
+    })
+  }
+
   const inputBoxHeight = 126
   const outputBoxHeight = 170
 
@@ -79,7 +99,7 @@ function ActionCard({ action }: { action: Action }) {
                   Input
                 </div>
                 <div style={{height: inputBoxHeight}}>
-                  <AmountInput amount={action.input} handleChange={handleInputQuantityChange}/>
+                  <AmountInput amount={action.input} handleQuantityChange={handleInputQuantityChange} handleTokenChange={handleInputTokenChange}/>
                 </div>
               </div>
             ) : (
@@ -97,7 +117,7 @@ function ActionCard({ action }: { action: Action }) {
                   Output
                 </div>
                 <div style={{height: inputBoxHeight}}>
-                  <AmountInput amount={action.output} handleChange={handleOutputQuantityChange}/>
+                  <AmountInput amount={action.output} handleQuantityChange={handleOutputQuantityChange} handleTokenChange={handleOutputTokenChange}/>
                 </div>
               </div>
             ) : (
