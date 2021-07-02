1a082761616dc93c7cef4fd70dd3bd5a7ddc1b0c
Jason Chitla <jasonchitla@hotmail.com>
2021-07-01 23:32:01 -0400

    fixed horizontal scrollbar

--- packages/frontend/components/ActionsRow/index.tsx
+++ packages/frontend/components/ActionsRow/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import useTreasure from '../../hooks/useTreasure'
 import ActionCard from './ActionCard'
 
-function ActionsRow() {
+function ActionsRow({ style }) {
   const { actions } = useTreasure()
 
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex flex-row justify-center" style={style}>
       {actions.map((action) => {
         return <ActionCard key={action.id} action={action} />
       })}
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -8,9 +8,20 @@ import Button, {
 import React from 'react'
 import ActionModal from '@components/ActionModal/ActionModal'
 import { useState } from 'react'
+import useTreasure from 'hooks/useTreasure'
 
 export default function Home() {
   const [actionModalState, setActionModalState] = useState(false)
+  const { actions } = useTreasure()
+
+  const computeWidth = () => {
+    if (!actions.length) {
+      return '50%'
+    }
+
+    const buttonWidth = 96
+    return 295 * actions.length + buttonWidth
+  }
 
   return (
     <div className="bg-darkBlue h-screen w-screen">
@@ -20,19 +31,20 @@ export default function Home() {
         ''
       )}
       <NavBar />
-      <div className="flex flex-row justify-center items-center mt-48">
-        <div className="w-3/4 overflow-x-scroll scrollbar scrollbar-thumb-offWhite scrollbar-track-darkerBlue pb-12">
-          <ActionsRow />
-        </div>
-        <div className="ml-24">
-          <Button
-            size={ButtonSize.ExtraLarge}
-            buttonType={ButtonType.Primary}
-            buttonShape={ButtonShape.Circular}
-            onClick={() => setActionModalState(true)}
-          >
-            +
-          </Button>
+      <div className="flex flex-row justify-center mt-48">
+        <div className="flex flex-row w-5/6 items-center scrollbar scrollbar-thumb-offWhite scrollbar-track-darkerBlue pb-12"
+          style={{ height: 501 }}>
+          <ActionsRow style={{ minWidth: computeWidth() }}/>
+          <div className="mt-14">
+            <Button
+              size={ButtonSize.ExtraLarge}
+              buttonType={ButtonType.Primary}
+              buttonShape={ButtonShape.Circular}
+              onClick={() => setActionModalState(true)}
+            >
+              +
+            </Button>
+          </div>
         </div>
       </div>
     </div>
