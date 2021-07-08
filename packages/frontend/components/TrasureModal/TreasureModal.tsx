66ec61c75306b1da298995122c886ae6c25a7c24
Jason Chitla <jasonchitla@hotmail.com>
2021-07-08 19:05:28 -0400

    extracted treasureMaps into context

--- packages/frontend/components/TrasureModal/TreasureModal.tsx
+++ packages/frontend/components/TrasureModal/TreasureModal.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import useEthers from 'hooks/useEthers'
 import useTreasure from 'hooks/useTreasure'
-
 import TreasureManagement from './TreasureManagement'
 import TreasureMaps from './TreasureMaps'
 
 export default function TreasureModal({ toggle }) {
   const { treasureAddress } = useEthers()
-  const { actualTreasureAmounts } = useTreasure()
-  const treasureMaps = [
-    {
-      id: '0x3E259685A7b778dE2a8DbdF836659c4011cCAb9e',
-    },
-    {
-      id: '0x3E259685A7b778dE2a8DbdF836659c4011cCAb9e',
-    },
-  ]
+  const { actualTreasureAmounts, treasureMaps } = useTreasure()
 
   return (
     <div className="bg-black bg-opacity-75 h-screen w-screen text-white flex justify-center items-center fixed z-10">
